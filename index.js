12ed2d10d84c4ee0609043effc591210b0fb7d8a
roy <kimathiwaweru@gmail.com>
2022-11-22 19:24:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Write your solution here!
+const cats=[
+    "Milo", 
+    "Otis", 
+    "Garfield",
+];
+function beforeEach() {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+    return cats;
+  };
+
+function destructivelyAppendCat (name) {
+    beforeEach();
+
+    cats.push(name);
+    return cats;
+};
+
+function destructivelyPrependCat (name){
+    beforeEach();
+
+    cats.unshift(name);
+    return cats;
+};
+
+function destructivelyRemoveLastCat (name){
+    beforeEach();
+
+    cats.pop(name);
+    return cats;
+};
+function destructivelyRemoveFirstCat (name){
+    beforeEach();
+
+    cats.shift(name);
+    return cats;
+};
+
+function appendCat(name){
+    const altCats = [...cats, `${name}`];
+    return altCats;
+}
+
+function prependCat(name){
+    const preCats = [`${name}`,...cats ];
+    return preCats;
+}
+function removeLastCat(name){
+    const rvmLstCat = cats.slice(0, cats.length - 1);
+    return rvmLstCat;
+}
+function removeFirstCat(name){
+    const rvmFstCat = cats.slice(1);
+    return rvmFstCat;
+}
